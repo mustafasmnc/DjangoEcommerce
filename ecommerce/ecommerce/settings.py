49e76f526d2f372c97feaec7cc5dd5c984c0b744
Mustafa <mustafasmnc@gmail.com>
2020-12-21 20:55:49 +0300

    Product ImageField()

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,9 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
 
-import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
